3c495553f9fb4fce496fac114eb3eede968fe844
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-06-10 16:57:55 -0300

    feat: enviar dados

---
+++ model/DAO/categoria/categoria.js
@@ -0,0 +1,6 @@
+/****************************************************************************************
+ * Objetivo: Arquivo responsável pelo CRUD de dados de categoria no banco de dados MySQL.
+ * Autor: Gustavo Vidal de Abreu
+ * Data: 10/06/2026 (quarta-feira)
+ * Versão: 1.0
+****************************************************************************************/
